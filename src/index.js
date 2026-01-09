557fc6387104d89d5801b361e909400e543ed1fa
jellomoon <damonita@gmail.com>
2026-01-09 09:57:43 +0100

    Added basic JS form handling, added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "From fairest creatures we desire increase",
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
+
+let promptInputElement = document.querySelector("#prompt-input-form");
+promptInputElement.addEventListener("submit", generatePoem);
