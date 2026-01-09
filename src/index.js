6f452b974ec4d94aa98395da3e336c450b092716
jellomoon <damonita@gmail.com>
2026-01-09 11:07:23 +0100

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("Poem generated");
   new Typewriter("#poem", {
-    strings: "From fairest creatures we desire increase",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: null,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let userInput = document.querySelector("#user-input");
+  let apiKey = "0ef1f93t64c8bao485ffda3401a92d04";
+  let context =
+    "You are a robot poet expert who loves Shakespeare. Your mission is to generate a beautiful poem in basic HTML. The poem can be 4, 5, or 6 lines long. Make sure to follow the user instructions. Because you are a robot you always include something robot, computer, or machinery related in you poems. Separeate each line with a <br />. Sign each poem at the bottom with '- Robot Bard' inside a <strong> element. Show nothing but the poem in your output. Hide the '```html' at the beginning and '```' at the end. ";
+  let prompt = `User Instructions: Generate a poem about ${userInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`${context}`);
+  console.log(`${prompt}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let promptInputElement = document.querySelector("#prompt-input-form");
